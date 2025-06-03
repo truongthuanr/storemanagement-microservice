de739523f3a598caa2f6f0b1e26548d2bdae00b6
truongthuanr <truongthuan.r@gmail.com>
2025-06-04 00:09:34 +0700

    update grpc issue, docker file

--- product-service/app/main.py
+++ product-service/app/main.py
@@ -1,6 +1,14 @@
+
+import sys
+import os
+BASE_DIR = os.path.dirname(__file__)
+# Thêm thư mục 'proto/' vào sys.path
+PROTO_DIR = os.path.join(BASE_DIR, "proto")
+sys.path.append(PROTO_DIR)
+
 from fastapi import FastAPI
 from app.api.product_router import router as product_router
-from app.db.database import Base, engine
+from app.database.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
