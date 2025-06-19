018e56561196b37659a130f3951780c9e7a88051
truongthuanr <truongthuan.r@gmail.com>
2025-06-19 22:48:13 +0700

    fix order service

--- order-service/app/database/database.py
+++ order-service/app/database/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from app.config import settings  # load từ .env hoặc config
 from sqlalchemy.orm import declarative_base
 
@@ -8,3 +8,11 @@ Base = declarative_base()
 DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
