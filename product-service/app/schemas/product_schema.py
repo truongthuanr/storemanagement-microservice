1744e82b311906fb8e23cf1055b5dd58d8c5181d
truongthuanr <truongthuan.r@gmail.com>
2025-07-25 14:22:35 +0700

    resolve conflict

--- product-service/app/schemas/product_schema.py
+++ product-service/app/schemas/product_schema.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from pydantic import BaseModel
 
 class ProductBase(BaseModel):
@@ -16,22 +15,3 @@ class ProductOut(ProductBase):
 
     class Config:
         from_attributes = True
-=======
-from pydantic import BaseModel
-
-class ProductBase(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    category: str
-
-class ProductCreate(ProductBase):
-    pass
-
-class ProductOut(ProductBase):
-    id: int
-    is_active: bool
-
-    class Config:
-        from_attributes = True
->>>>>>> Stashed changes
